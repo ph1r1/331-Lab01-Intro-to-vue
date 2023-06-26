c21328ca51e7df44e809bc9654b2292fc3af3875
webwebweb5 <phiriyakornman@gmail.com>
2023-06-26 22:01:52 +0700

    Step 10.3-10.4

--- main.js
+++ main.js
@@ -2,11 +2,10 @@ const { createApp, ref, computed } = Vue;
 
 const app = createApp({
   setup() {
-    const cart = ref(0);
+    const cart = ref([]);
     const premium = ref(false);
-    const updateCart = () => {
-      cart.value += 1;
-      //   cart.value.push(id);
+    const updateCart = (id) => {
+      cart.value.push(id);
       // console.log(cart.value);
     };
     return {
