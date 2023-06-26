0106d60684c45e7bc9c5d15e23aff7fecabedcf2
webwebweb5 <phiriyakornman@gmail.com>
2023-06-26 20:47:12 +0700

    Step 3.2 and 3.3

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ const { createApp, ref, computed } = Vue;
 createApp({
   setup() {
     const product = ref("Boots");
+    const image = ref("./assets/images/socks_green.jpg");
     return {
       product,
+      image,
     };
   },
 }).mount("#app");
