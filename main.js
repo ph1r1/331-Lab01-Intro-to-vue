512f73452dd740c3b28dbed7dcb1ce7dabb61df3
webwebweb5 <phiriyakornman@gmail.com>
2023-06-26 20:59:09 +0700

    Step 5.1-5.2

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ createApp({
     const product = ref("Boots");
     const image = ref("./assets/images/socks_green.jpg");
     const inStock = ref(true);
-    const inventory = ref(5);
-    const onSale = ref(true);
+    const inventory = ref(100);
+    const details = ref(["50% cotton", "30% wool", "20% polyester"]);
     return {
       product,
       image,
       inStock,
       inventory,
-      onSale,
+      details,
     };
   },
 }).mount("#app");
