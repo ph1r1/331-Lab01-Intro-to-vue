35ff95bf0d1594a0e5a0ca2ac9550dcaaff02c36
webwebweb5 <phiriyakornman@gmail.com>
2023-06-26 21:01:05 +0700

    Step 5.3-5.4

--- main.js
+++ main.js
@@ -7,12 +7,23 @@ createApp({
     const inStock = ref(true);
     const inventory = ref(100);
     const details = ref(["50% cotton", "30% wool", "20% polyester"]);
+    const variants = ref([
+      {
+        id: 2234,
+        color: "green",
+      },
+      {
+        id: 2235,
+        color: "blue",
+      },
+    ]);
     return {
       product,
       image,
       inStock,
       inventory,
       details,
+      variants,
     };
   },
 }).mount("#app");
