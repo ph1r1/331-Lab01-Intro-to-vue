51cdba2771f5f5c6d45685f06ebde31f6e07db7e
webwebweb5 <phiriyakornman@gmail.com>
2023-06-26 20:49:29 +0700

    Step 3.6*

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ createApp({
   setup() {
     const product = ref("Boots");
     const image = ref("./assets/images/socks_green.jpg");
+    const link = ref("https://www.camt.cmu.ac.th/");
     return {
       product,
       image,
+      link,
     };
   },
 }).mount("#app");
