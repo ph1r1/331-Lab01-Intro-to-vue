01072806d9b73f2dfaa6ba47fb9e6598edcc8bc2
webwebweb5 <phiriyakornman@gmail.com>
2023-06-26 21:40:51 +0700

    Step 9.1-9.6

--- main.js
+++ main.js
@@ -1,63 +1,14 @@
 const { createApp, ref, computed } = Vue;
 
-createApp({
+const app = createApp({
   setup() {
-    const product = ref("Boots");
-    const brand = ref("SE 331");
-    // const image = ref("./assets/images/socks_green.jpg");
-    // const inStock = ref(true);
-    const inventory = ref(100);
-    const details = ref(["50% cotton", "30% wool", "20% polyester"]);
-    const variants = ref([
-      {
-        id: 2234,
-        color: "green",
-        image: "./assets/images/socks_green.jpg",
-        quantity: 50,
-      },
-      {
-        id: 2235,
-        color: "blue",
-        image: "./assets/images/socks_blue.jpg",
-        quantity: 0,
-      },
-    ]);
-    const selectedVariant = ref(0);
-    const updateVariant = (index) => {
-      selectedVariant.value = index;
-    };
-    const image = computed(() => {
-      return variants.value[selectedVariant.value].image;
-    });
-    const inStock = computed(() => {
-      return variants.value[selectedVariant.value].quantity;
-    });
     const cart = ref(0);
-    const addToCart = () => {
-      cart.value += 1;
-    };
-    const title = computed(() => {
-      return brand.value + " " + product.value;
-    });
-    const updateImage = (variantImage) => {
-      image.value = variantImage;
-    };
-    const onSale = ref(true);
-    const onSaleMsg = computed(() => {
-        return onSale && title.value + " is on sale";
-      });
     return {
-      title,
-      image,
-      inStock,
-      inventory,
-      details,
-      variants,
       cart,
-      addToCart,
-      updateImage,
-      updateVariant,
-      onSaleMsg,
     };
   },
-}).mount("#app");
+});
+
+app.component("product-display", productDisplay);
+
+app.mount("#app");
