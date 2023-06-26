43e2cd4e1815f55ed204f6931ad33125d2bf1178
webwebweb5 <phiriyakornman@gmail.com>
2023-06-26 21:45:22 +0700

    Step 9.7-9.8

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -13,6 +13,7 @@ const productDisplay = {
         <p v-if="inventory > 10">In Stock</p>
         <p v-else-if="inventory <= 10 && inventory > 0">Almost Out of Stock</p>
         <p v-else>Out of Stock</p>
+        <p>Shipping: {{ shipping }}</p>
         <ul>
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
@@ -35,7 +36,17 @@ const productDisplay = {
       </div>
       </div>
       `,
-  setup() {
+  props: {
+    premium: Boolean,
+  },
+  setup(props) {
+    const shipping = computed(() => {
+      if (props.premium) {
+        return "Free";
+      } else {
+        return 30;
+      }
+    });
     const product = ref("Boots");
     const brand = ref("SE 331");
     // const image = ref("./assets/images/socks_green.jpg");
@@ -86,6 +97,7 @@ const productDisplay = {
       addToCart,
       updateImage,
       updateVariant,
+      shipping,
     };
   },
 };
