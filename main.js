4fca55eced22fda55d450b3199301649a69e86de
webwebweb5 <phiriyakornman@gmail.com>
2023-06-26 21:45:45 +0700

    Step 9.9

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { createApp, ref, computed } = Vue;
 const app = createApp({
   setup() {
     const cart = ref(0);
-    const premium = ref(true);
+    const premium = ref(false);
     return {
       cart,
       premium,
