5ed7fab6d2b694b9ffbdd347c4e0b116b6e6de8f
webwebweb5 <phiriyakornman@gmail.com>
2023-06-26 21:55:29 +0700

    Step 9.10

--- main.js
+++ main.js
@@ -4,13 +4,16 @@ const app = createApp({
   setup() {
     const cart = ref(0);
     const premium = ref(false);
+    const details = ref(["50% cotton", "30% wool", "20% polyester"]);
     return {
       cart,
       premium,
+      details,
     };
   },
 });
 
 app.component("product-display", productDisplay);
+app.component("product-details", productDetails);
 
 app.mount("#app");
