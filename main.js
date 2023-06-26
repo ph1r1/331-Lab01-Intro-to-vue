18f9160f602f6a51e713862463109887459f7be7
webwebweb5 <phiriyakornman@gmail.com>
2023-06-26 20:43:48 +0700

    Step 2.5*

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ const { createApp, ref, computed } = Vue;
 createApp({
   setup() {
     const product = ref("Boots");
+    const description = ref("these are boots!");
     return {
       product,
+      description,
     };
   },
 }).mount("#app");
